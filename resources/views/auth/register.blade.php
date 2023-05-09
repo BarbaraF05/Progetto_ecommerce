63a31810ef053fffbb2a96389069dbb4980068d4
Gabriele Nucci <gabryelenucci@gmail.com>
2023-05-09 16:27:09 +0200

    Inizio progetto con Scaffalging e Auth

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Registrati</h1>
+                <form method="POST" action='{{route('register')}}'>
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">UserName</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Conferma Password</label>
+                        <input type="password" class="form-control" name="password_confirmation">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Registrati</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-layout>
