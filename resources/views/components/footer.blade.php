bb4fe587f5aea27163c316e7946606dca8e44f3d
RossellaCln <r.celano90@gmail.com>
2023-05-11 18:41:06 +0200

    fine US3

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid mt-4 p-5 bg-footer text-white">
         <div class="row">
             <div class="col-12 text-center">
-                <p>Presto.it</p>
+                <img src="../../img/logo-bianco.png" alt="Logo" width="90" height="80" class="d-inline-block align-text-top">
                 <p>Vuoi lavorare con noi?</p>
                 <p>Registrati e clicca qui!</p>
                 <a href="{{route('become.revisor')}}" class="btn btn-warning text-white shadow my-3">Diventa revisore</a>
